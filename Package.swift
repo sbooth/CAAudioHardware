7fac3c44dec16a516932eaab88ef7a8c01cb6ee4
Stephen F. Booth <me@sbooth.org>
2023-10-12 16:24:51 -0500

    Add linked frameworks

--- Package.swift
+++ Package.swift
@@ -18,7 +18,11 @@ let package = Package(
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
 		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
-			name: "CAAudioHardware"),
+			name: "CAAudioHardware",
+			linkerSettings: [
+				.linkedFramework("CoreAudio"),
+				.linkedFramework("AVFAudio"),
+			]),
 		.testTarget(
 			name: "CAAudioHardwareTests",
 			dependencies: ["CAAudioHardware"]),
