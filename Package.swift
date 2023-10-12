d08b8b1fcd6d78cc09c011891997b79d832f9b02
Stephen F. Booth <me@sbooth.org>
2023-10-12 12:44:13 -0500

    Spaces to tabs

--- Package.swift
+++ Package.swift
@@ -4,30 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "CAAudioHardware",
+	name: "CAAudioHardware",
 	platforms: [
 		.macOS(.v10_15),
 		.iOS(.v12)
 	],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "CAAudioHardware",
-            targets: ["CAAudioHardware"]),
-    ],
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.library(
+			name: "CAAudioHardware",
+			targets: ["CAAudioHardware"]),
+	],
 	dependencies: [
 		.package(url: "https://github.com/sbooth/SFBAudioUtilities", branch: "spm")
 	],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
 			name: "CAAudioHardware",
 			dependencies: [
 				.product(name: "CoreAudioExtensions", package: "SFBAudioUtilities")
 			]),
-        .testTarget(
-            name: "CAAudioHardwareTests",
-            dependencies: ["CAAudioHardware"]),
-    ]
+		.testTarget(
+			name: "CAAudioHardwareTests",
+			dependencies: ["CAAudioHardware"]),
+	]
 )
