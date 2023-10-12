72319918dcc160d808beb82c8bf0aac5197231f0
Stephen F. Booth <me@sbooth.org>
2023-10-12 13:10:26 -0500

    Remove iOS from platforms

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
 	name: "CAAudioHardware",
 	platforms: [
 		.macOS(.v10_15),
-		.iOS(.v12)
 	],
 	products: [
 		// Products define the executables and libraries a package produces, making them visible to other packages.
