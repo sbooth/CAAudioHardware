b3b7f82941c7878b7ad0f8da82ba51a805ccb910
Stephen Booth <me@sbooth.org>
2024-03-31 10:56:04 -0500

    Add docc workflow for GH Pages (#11)
    * Add swift-docc-plugin dependency
    
    * Create docc.yaml
    
    * Update docc.yaml

--- Package.swift
+++ Package.swift
@@ -14,6 +14,11 @@ let package = Package(
 			name: "CAAudioHardware",
 			targets: ["CAAudioHardware"]),
 	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+		.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
+	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
 		// Targets can depend on other targets in this package and products from dependencies.
