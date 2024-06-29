e885f2c12e52ce27df145e7e2de09cdbebc5060d
Stephen Booth <me@sbooth.org>
2024-06-29 16:49:14 -0500

    Refactor structs (#33)

---
+++ Sources/CAAudioHardware/AudioDevice+PlayThroughDestination.swift
@@ -0,0 +1,34 @@
+//
+// Copyright © 2020-2024 Stephen F. Booth <me@sbooth.org>
+// Part of https://github.com/sbooth/CAAudioHardware
+// MIT license
+//
+
+import Foundation
+import CoreAudio
+
+extension AudioDevice {
+	/// A play-through destination for an audio device
+	public struct PlayThroughDestination: Equatable, Hashable/*, Sendable*/ {
+		/// Returns the owning audio device
+		public let device: AudioDevice
+		/// Returns the play-through destination ID
+		public let id: UInt32
+
+		/// Returns the play-through destination name
+		public func name() throws -> String {
+			return try device.nameOfPlayThroughDestination(id)
+		}
+	}
+}
+
+extension AudioDevice.PlayThroughDestination: CustomDebugStringConvertible {
+	// A textual representation of this instance, suitable for debugging.
+	public var debugDescription: String {
+		if let name = try? name() {
+			return "<\(type(of: self)): '\(id.fourCC)' \"\(name)\" on \(device.debugDescription)>"
+		} else {
+			return "<\(type(of: self)): '\(id.fourCC)' on \(device.debugDescription))>"
+		}
+	}
+}
