3e4fba57c8c52e91d7a7259998a0654af23c0bdb
Stephen F. Booth <me@sbooth.org>
2024-07-05 10:51:59 -0500

    Add simply UnfairLock tests

--- Tests/CAAudioHardwareTests/CAAudioHardwareTests.swift
+++ Tests/CAAudioHardwareTests/CAAudioHardwareTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import CAAudioHardware
 
 final class CAAudioHardwareTests: XCTestCase {
-    func testDevices() throws {
+	func testDevices() throws {
 		let devices = try AudioDevice.devices
 		for device in devices {
 			let ownedObjects = try device.ownedObjects
@@ -12,9 +12,24 @@ final class CAAudioHardwareTests: XCTestCase {
 			}
 
 			_ = try device.controlList
-			
+
 			_ = try device.streams(inScope: .output)
 			_ = try device.streams(inScope: .input)
 		}
-    }
+	}
+
+	func testUnfairLockCopying() {
+		let lock = UnfairLock()
+		let copy = lock
+		XCTAssertIdentical(lock.storage, copy.storage)
+	}
+
+	func testUnfairLockOwnership() {
+		let lock = UnfairLock()
+		lock.precondition(.notOwner)
+		lock.lock()
+		lock.precondition(.owner)
+		lock.unlock()
+		lock.precondition(.notOwner)
+	}
 }
