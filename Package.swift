216a149f31d3fa2743d236215c2b39c83a1e45a9
Stephen F. Booth <me@sbooth.org>
2024-07-07 21:33:33 -0500

    Update formatting

--- Package.swift
+++ Package.swift
@@ -9,14 +9,13 @@ let package = Package(
 		.macOS(.v10_15),
 	],
 	products: [
-		// Products define the executables and libraries a package produces, making them visible to other packages.
 		.library(
 			name: "CAAudioHardware",
-			targets: ["CAAudioHardware"]),
+			targets: [
+				"CAAudioHardware",
+			]),
 	],
 	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
 			name: "CAAudioHardware",
 			linkerSettings: [
@@ -25,6 +24,8 @@ let package = Package(
 			]),
 		.testTarget(
 			name: "CAAudioHardwareTests",
-			dependencies: ["CAAudioHardware"]),
+			dependencies: [
+				"CAAudioHardware",
+			]),
 	]
 )
