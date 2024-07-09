7d98edbff5a87dec4ab69be7b9520742824718b7
Stephen F. Booth <me@sbooth.org>
2024-07-09 10:55:32 -0500

    Add AudioTap

---
+++ Sources/CAAudioHardware/AudioTap.swift
@@ -0,0 +1,109 @@
+//
+// Copyright © 2020-2024 Stephen F. Booth <me@sbooth.org>
+// Part of https://github.com/sbooth/CAAudioHardware
+// MIT license
+//
+
+import Foundation
+import CoreAudio
+
+/// A HAL audio tap object
+///
+/// This class has a single scope (`kAudioObjectPropertyScopeGlobal`) and a single element (`kAudioObjectPropertyElementMain`)
+/// - remark: This class correponds to objects with base class `kAudioTapClassID`
+@available(macOS 14.2, *)
+public class AudioTap: AudioObject {
+	/// Returns the available audio taps
+	/// - remark: This corresponds to the property`kAudioHardwarePropertyTapList` on `kAudioObjectSystemObject`
+	public static var taps: [AudioTap] {
+		get throws {
+			// Revisit if a subclass of `AudioTap` is added
+			try getAudioObjectProperty(PropertyAddress(kAudioHardwarePropertyTapList), from: AudioObjectID(kAudioObjectSystemObject)).map { AudioTap($0) }
+		}
+	}
+
+	/// Returns an initialized `AudioTap` with `uid` or `nil` if unknown
+	/// - remark: This corresponds to the property `kAudioHardwarePropertyTranslateUIDToTap` on `kAudioObjectSystemObject`
+	/// - parameter uid: The UID of the desired tap
+	public class func makeTap(forUID uid: String) throws -> AudioTap? {
+		var qualifier = uid as CFString
+		let objectID: AudioObjectID = try getAudioObjectProperty(PropertyAddress(kAudioHardwarePropertyTranslateUIDToTap), from: AudioObjectID(kAudioObjectSystemObject), qualifier: PropertyQualifier(&qualifier))
+		guard objectID != kAudioObjectUnknown else {
+			return nil
+		}
+
+		// Revisit if a subclass of `AudioTap` is added
+		return AudioTap(objectID)
+	}
+
+	// A textual representation of this instance, suitable for debugging.
+	public override var debugDescription: String {
+		do {
+			return "<\(type(of: self)): 0x\(objectID.hexString) \(try uid)>"
+		} catch {
+			return super.debugDescription
+		}
+	}
+}
+
+@available(macOS 14.2, *)
+extension AudioTap {
+	/// Returns the UID
+	/// - remark: This corresponds to the property `kAudioTapPropertyUID`
+	public var uid: String {
+		get throws {
+			try getProperty(PropertyAddress(kAudioTapPropertyUID), type: CFString.self) as String
+		}
+	}
+
+	/// Returns the description
+	/// - remark: This corresponds to the property `kAudioTapPropertyDescription`
+	public var description: CATapDescription {
+		get throws {
+			try getProperty(PropertyAddress(kAudioTapPropertyDescription))
+		}
+	}
+
+	/// Returns the format
+	/// - remark: This corresponds to the property `kAudioTapPropertyFormat`
+	public var format: AudioStreamBasicDescription {
+		get throws {
+			try getProperty(PropertyAddress(kAudioTapPropertyFormat))
+		}
+	}
+}
+
+@available(macOS 14.2, *)
+extension AudioTap {
+	/// Returns `true` if `self` has `selector`
+	/// - parameter selector: The selector of the desired property
+	public func hasSelector(_ selector: AudioObjectSelector<AudioTap>) -> Bool {
+		return hasProperty(PropertyAddress(PropertySelector(selector.rawValue)))
+	}
+
+	/// Returns `true` if `selector` is settable
+	/// - parameter selector: The selector of the desired property
+	/// - throws: An error if `self` does not have the requested property
+	public func isSelectorSettable(_ selector: AudioObjectSelector<AudioTap>) throws -> Bool {
+		return try isPropertySettable(PropertyAddress(PropertySelector(selector.rawValue)))
+	}
+
+	/// Registers `block` to be performed when `selector` changes
+	/// - parameter selector: The selector of the desired property
+	/// - parameter queue: An optional dispatch queue on which `block` will be invoked.
+	/// - parameter block: A closure to invoke when the property changes or `nil` to remove the previous value
+	/// - throws: An error if the property listener could not be registered
+	public func whenSelectorChanges(_ selector: AudioObjectSelector<AudioTap>, on queue: DispatchQueue? = nil, perform block: PropertyChangeNotificationBlock?) throws {
+		try whenPropertyChanges(PropertyAddress(PropertySelector(selector.rawValue)), on: queue, perform: block)
+	}
+}
+
+@available(macOS 14.2, *)
+extension AudioObjectSelector where T == AudioTap {
+	/// The property selector `kAudioTapPropertyUID`
+	public static let uid = AudioObjectSelector(kAudioTapPropertyUID)
+	/// The property selector `kAudioTapPropertyDescription`
+	public static let description = AudioObjectSelector(kAudioTapPropertyDescription)
+	/// The property selector `kAudioTapPropertyFormat`
+	public static let format = AudioObjectSelector(kAudioTapPropertyFormat)
+}
