c7a4f546933ba4ce1f5e79701f2d9ee60182b00b
Stephen F. Booth <me@sbooth.org>
2024-07-17 06:57:53 -0500

    Add AudioObjectID+systemObject.swift

---
+++ Sources/CAAudioHardware/AudioObjectID+systemObject.swift
@@ -0,0 +1,13 @@
+//
+// Copyright © 2024 Stephen F. Booth <me@sbooth.org>
+// Part of https://github.com/sbooth/CAAudioHardware
+// MIT license
+//
+
+import Foundation
+import CoreAudio
+
+extension AudioObjectID {
+	/// The system audio object ID (`kAudioObjectSystemObject`)
+	static let systemObject = AudioObjectID(kAudioObjectSystemObject)
+}
