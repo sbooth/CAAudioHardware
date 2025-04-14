12938582dd7602538e4450ed4ed2586e9ec4836c
Stephen F. Booth <me@sbooth.org>
2025-04-13 21:07:21 -0500

    Update year

--- Sources/CAAudioHardware/AudioTap.swift
+++ Sources/CAAudioHardware/AudioTap.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2020-2024 Stephen F. Booth <me@sbooth.org>
+// Copyright © 2020-2025 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/CAAudioHardware
 // MIT license
 //
--- Sources/CAAudioHardware/UnfairLock.swift
+++ Sources/CAAudioHardware/UnfairLock.swift
@@ -1,5 +1,5 @@
 //
-// Copyright © 2018-2024 Stephen F. Booth <me@sbooth.org>
+// Copyright © 2018-2025 Stephen F. Booth <me@sbooth.org>
 // Part of https://github.com/sbooth/CAAudioHardware
 // MIT license
 //
